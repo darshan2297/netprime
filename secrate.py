f3bc2a829604ba9b924ed9dedf0f98e3f2f8cb51
darshan2297 <darshanvankawala2297@gmail.com>
2020-05-05 19:19:10 +0530

    fourth commit

--- secrate.py
+++ secrate.py
@@ -2,7 +2,7 @@ import os
 
 def vars():
     os.environ['EMAIL_USER'] = 'darshanvankawala2297@gmail.com'
-    os.environ['EMAIL_PASSWORD'] = 'iloveumom@2297'
+    os.environ['EMAIL_PASSWORD'] = 'nisha@2208'
     os.environ['SECRET_KEY'] = 'h7c&k32xug#%-fev8@gu7h6nk1@0wg+7n*9_3#qj-7bs*q%1ud'
     os.environ['AWS_ACCESS_KEY_ID'] = 'AKIA3Y6I22A5QE64J2NB'
     os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
