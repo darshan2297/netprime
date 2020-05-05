a74ae72672d544d45b5038f25bd3d7b98df075de
darshan2297 <darshanvankawala2297@gmail.com>
2020-05-05 16:21:56 +0530

    fourth commit

--- Videosubscription/settings.py
+++ Videosubscription/settings.py
@@ -95,10 +95,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
